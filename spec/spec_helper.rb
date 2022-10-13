d6d9e7b9a5d1f6ad311d439e096301dd697418fd
Alex Psitos <apsitos08@gmail.com>
2022-10-13 09:21:56 -0400

    Feat: add simplecov to ensure adequate test coverage

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -13,6 +13,10 @@
 # it.
 #
 require 'webmock/rspec'
+require 'simplecov'
+SimpleCov.start do 
+  add_filter 'spec'
+end
 # See https://rubydoc.info/gems/rspec-core/RSpec/Core/Configuration
 RSpec.configure do |config|
   # rspec-expectations config goes here. You can use an alternate
