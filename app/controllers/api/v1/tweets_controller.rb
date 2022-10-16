5c13333e9e9fc8d21e172f3ce80222a43b8b7365
Alex Psitos <apsitos08@gmail.com>
2022-10-16 12:04:37 -0400

    Refactor: index action and spec refactored to reflect new scope of same

--- app/controllers/api/v1/tweets_controller.rb
+++ app/controllers/api/v1/tweets_controller.rb
@@ -5,29 +5,20 @@ class Api::V1::TweetsController < ApplicationController
 
   def index
     if params[:query]
-      response = TweetFacade.find_by_topic(params[:query])
-      render json: TweetSerializer.live_tweets(response)
+      render json: TweetSerializer.database_tweets(Tweet.where(subject: params[:query]))
     else
-      render json: TweetSerializer.database_tweets(Tweet.all)
+      return not_found 
     end
   end 
 
-  def show
-    if Tweet.exists?(params[:id])
-      render json: TweetSerializer.single_tweet(Tweet.find(params[:id]))
-    else
-      return not_found
-    end
-  end 
-  
-  
+    
   private 
     def tweet_params
       params.permit(:created_at, :text, :edit_history_tweet_ids, :query, :subject)
     end
 
     def not_found 
-      render json: { data: {message: 'No tweet matches this id'} },
+      render json: { data: {message: 'No tweets found with that subject'} },
       status: 404
     end
 end 
--- app/facades/tweet_facade.rb
+++
@@ -1,6 +0,0 @@
-class TweetFacade
-
-  def self.find_by_topic(topic)
-    TwitterService.get_recent_tweets(topic)
-  end
-end
